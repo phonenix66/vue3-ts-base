54baaa0395b3b5a720481195e41ea2f84767a200
phonenix66 <37186187@163.com>
2021-02-25 10:32:39 +0800

    init

---
+++ src/views/AboutMe.vue
@@ -0,0 +1,37 @@
+<template>
+  <div ref="root" class="about">
+    <p>about-me</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref, onMounted } from 'vue';
+
+const AboutMe = defineComponent({
+  setup() {
+    onMounted(() => {
+      console.log('mounted is creatd');
+    });
+    const root = ref(null);
+    return { root };
+  },
+  data() {
+    return {
+      book: {
+        pageName: 'Chapter 1',
+        author: 'swift'
+      }
+    };
+  },
+  methods: {
+    caculatePageName(): string {
+      return this.book.pageName
+        .split('-')
+        .reverse()
+        .join();
+    }
+  }
+});
+
+export default AboutMe;
+</script>
---
+++ src/views/Contact.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    contact.page
+  </div>
+</template>
+
+<script lang="ts">
+// import HelloWorld from '@/components/HelloWorld1.vue' // @ is an alias to /src
+import { defineComponent } from 'vue';
+
+const Home = defineComponent({});
+
+export default Home;
+</script>
